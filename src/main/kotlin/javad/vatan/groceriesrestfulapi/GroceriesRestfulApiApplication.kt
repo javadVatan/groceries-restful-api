8755e8cd999b5aa822eb6dfc1ae8f8f8dfc3fef3
javad <jvatan75@gmail.com>
2026-04-23 17:03:36 +0330

    Initial commit

---
+++ src/main/kotlin/javad/vatan/groceriesrestfulapi/GroceriesRestfulApiApplication.kt
@@ -0,0 +1,11 @@
+package javad.vatan.groceriesrestfulapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GroceriesRestfulApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<GroceriesRestfulApiApplication>(*args)
+}
---
+++ src/test/kotlin/javad/vatan/groceriesrestfulapi/GroceriesRestfulApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package javad.vatan.groceriesrestfulapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GroceriesRestfulApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
